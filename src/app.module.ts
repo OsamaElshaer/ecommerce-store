08f31137f4aee1b2b6d1ec72fb8494981c8a3fa3
osamaelshaer <osamabinelshaer@gmail.com>
2026-09-09 14:06:39 +0300

    feat: add env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { envValidationSchema } from './config/env.validation';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT ?? '5432', 10),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: process.env.NODE_ENV === 'development',
-    }),
-  ],
-  controllers: [],
-  providers: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            validationSchema: envValidationSchema,
+            validationOptions: {
+                abortEarly: false, 
+            },
+        }),
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => ({
+                type: 'postgres',
+                host: config.get<string>('POSTGRES_HOST'),
+                port: config.get<number>('POSTGRES_PORT'),
+                username: config.get<string>('POSTGRES_USER'),
+                password: config.get<string>('POSTGRES_PASSWORD'),
+                database: config.get<string>('POSTGRES_DB'),
+                entities: [__dirname + '/**/*.entity{.ts,.js}'],
+                synchronize: config.get<string>('NODE_ENV') === 'development',
+            }),
+        }),
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
