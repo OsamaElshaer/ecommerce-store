e6b7e28ee9eac806137475c818454038b176fbad
osamaelshaer <osamabinelshaer@gmail.com>
2026-09-14 15:07:50 +0300

    chore: install dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { envValidationSchema } from './config/env.validation';
 import { HealthModule } from './health/health.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
     imports: [
@@ -24,6 +26,8 @@ import { HealthModule } from './health/health.module';
             }),
         }),
         HealthModule,
+        UsersModule,
+        AuthModule,
     ],
     controllers: [],
     providers: [],
--- src/config/env.validation.ts
+++ src/config/env.validation.ts
@@ -14,4 +14,6 @@ export const envValidationSchema = Joi.object({
 
     JWT_SECRET: Joi.string().required(),
     JWT_EXPIRES_IN: Joi.string().required(),
+    JWT_REFRESH_SECRET: Joi.string().required(),
+    JWT_REFRESH_EXPIRES_IN: Joi.string().required(),
 });
